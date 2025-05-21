c3abe48db85612a5a10d42dbfad3e531b220b5b5
kiwsley <kiwsleyfreire@gmail.com>
2025-05-20 23:52:30 -0300

    analise de dados finalizando limpeza do dados

---
+++ Scikit Learn/__init_py/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+pasta_projeto = Path(r"C:\Users\Kiwsley\Documents\Ciencia de dados Hashtag\Scikit Learn")
+
+pasta_dados = pasta_projeto / "dados"
+
+
+dados_tratados = pasta_dados / 'diabets_tratado.parquet'
