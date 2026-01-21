03bee12311fb167408e883d63d967d636f0632bf
kiwsley <kiwsleyfreire@gmail.com>
2026-01-20 23:36:16 -0300

    pareto

--- estatisticas/auxiliares.py
+++ estatisticas/auxiliares.py
@@ -1,16 +1,23 @@
 import pandas as pd
 
-def tabela_distribuicao_frequencias(dataframe,coluna):
+def tabela_distribuicao_frequencias(dataframe,coluna,coluna_frequencia = False):
 
     df_estatistica = pd.DataFrame()
 
-    # quantidade de ocorrencia
-    df_estatistica['Frequencia']= dataframe[coluna].value_counts().sort_index()
-    # %
-    df_estatistica['frequencia_relativa']= dataframe[coluna].value_counts(normalize=True).sort_index()*100
-    #soma dos valores
-    df_estatistica['frequencia_acumulada']= df_estatistica['Frequencia'].cumsum()
-    #soma da frenquencia relativa
+    if coluna_frequencia:
+        df_estatistica['Frequencia']=dataframe[coluna]
+        df_estatistica['frequencia_relativa']=df_estatistica['Frequencia']/df_estatistica['Frequencia'].sum()
+
+
+    else:
+        # quantidade de ocorrencia
+        df_estatistica['Frequencia']= dataframe[coluna].value_counts().sort_index()
+        # %
+        df_estatistica['frequencia_relativa']= dataframe[coluna].value_counts(normalize=True).sort_index()
+        
+    #soma dos valores    
+    df_estatistica['frequencia_acumulada']= df_estatistica['Frequencia'].cumsum()   
+    #soma da frenquencia relativa  
     df_estatistica['frequencia_relativa_acumulada']= df_estatistica['frequencia_relativa'].cumsum()
 
 
