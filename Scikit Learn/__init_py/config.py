fbfdb1daabb3544044e6bda91786358d7795c35e
kiwsley <kiwsleyfreire@gmail.com>
2026-03-01 20:03:21 -0300

    sckit

--- Scikit Learn/__init_py/config.py
+++ Scikit Learn/__init_py/config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-pasta_projeto = Path(r"C:\Users\Kiwsley\Documents\Ciencia de dados Hashtag\Scikit Learn")
+pasta_projeto = Path(r"C:\Users\kiwsley.costa\Documents\hashtagtreinamentos\ciencia_de_dados\Scikit Learn")
 
 pasta_dados = pasta_projeto / "dados"
 
-
 dados_tratados = pasta_dados / 'diabets_tratado.parquet'
+dados_categorizados = pasta_dados / 'diabets_tratado.parquet'
